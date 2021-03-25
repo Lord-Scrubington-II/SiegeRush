5301f22d57e4942b6ad1b485f157925aeca04e49
Lord-Scrubington-II (Zane W.) <zane.z.wang@gmail.com>
2021-03-25 00:33:38 -0700

    Initial commit

---
+++ Assets/Scripts/CoordLabeler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CoordLabeler : MonoBehaviour
+{
+    TextMeshPro label;
+    Vector2Int coords = new Vector2Int();
+
+    void Awake()
+    {
+        label = this.GetComponent<TextMeshPro>();
+        DisplayCoords();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!Application.isPlaying)
+        {
+            DisplayCoords();
+        }
+    }
+
+    private void DisplayCoords()
+    {
+        coords.x = Mathf.RoundToInt(gameObject.transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coords.y = Mathf.RoundToInt(gameObject.transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+
+        label.text = $"({coords.x}, {coords.y})";
+
+        gameObject.transform.parent.name = coords.ToString();
+    }
+}
---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<WayPoint> path = new List<WayPoint>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintWaypointNames();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PrintWaypointNames()
+    {
+        foreach(var waypoint in path)
+        {
+            Debug.Log(waypoint.name);
+        }
+    }
+}
